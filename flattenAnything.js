0c347410f8cd1d13207e395e904c9997bd6611ff
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-09-26 13:28:50 -0400

    handleNested so later we can deNest the whole thing

--- flattenAnything.js
+++ flattenAnything.js
@@ -3,16 +3,26 @@ var arr = [7, 1, 2, 3, 4];
 
 var obj = { a: 7, b: 2, c: {value:3} };
 
+function handleNested(val){
+  return Array.isArray(val)
+    ? reduceArray(val) 
+    : 
+    ( typeof val === 'object' 
+      ? reduceObject(val) 
+      : val
+    )
+}
+
 function reduceArray (arr) { // previous Value, currentValue, currentIndex
   return arr.reduce( (pV, cV, cI) =>
-    { return pushKeyAndValue(pV, { key: cI, value: cV }, arrKey); },
+    { return updatePv(pV, { key: cI, value: cV }, arrKey); },
     {keys:[], values:[]}
   );
 }
 
 function reduceObject (obj) { // previous Value, currentValue, currentIndex
   return Object.keys(obj).reduce( (pV, cV, cI) =>
-    { return pushKeyAndValue(pV, { key: cV, value: obj[cV] }, objKey ); },
+    { return updatePv(pV, { key: cV, value: obj[cV] }, objKey ); },
     { keys:[], values:[] }
   );
 }
@@ -41,5 +51,7 @@ console.log()
 console.log('outputs')
 
 console.log('reduce obj:', reduceObject(obj))
+console.log('reduce obj:', handleNested(obj))
 console.log()
 console.log('reduce arr:', reduceArray(arr))
+console.log('reduce arr:', handleNested(arr))
