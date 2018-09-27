cec333c95ba6bb79a73a69079bdb422480317f14
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-09-27 16:04:37 -0400

    code cleanup + rename function commonMap2 to commonReduceLogic

--- flattenAnything.js
+++ flattenAnything.js
@@ -30,13 +30,11 @@ function reduceArray (arr, oldKey) { // previous Value, currentValue, currentInd
   let newKeysAndValues = arr.reduce( (pV, cV, cI) =>
     {
       let key = cI; let value = cV;
-      pV = commonMap2({key, oldKey, value, pV}, `${oldKey}[${key}]`, `[${key}]`)
+      pV = commonReduceLogic({key, oldKey, value, pV}, `${oldKey}[${key}]`, `[${key}]`)
       return pV
     },
-    {}
+    {} // {keys:[],values:[]}
   );
-  // newKeysAndValue{ keys:[value], values:[3] }
-  // oldKeysAndValue{ keys:[a, b, c], values:[7, 2, {}] }
   return newKeysAndValues
 }
 
@@ -44,38 +42,31 @@ function reduceObject (obj, oldKey) { // previous Value, currentValue, currentIn
   let newKeysAndValues = Object.keys(obj).reduce( (pV, cV, cI) =>
     {
       let key = cV; let value = obj[cV]
-      pV = commonMap2({key, oldKey, value, pV}, `${oldKey}.${key}`, `.${key}`)
+      pV = commonReduceLogic({key, oldKey, value, pV}, `${oldKey}.${key}`, `.${key}`)
       return pV
     },
-    {}
+    {} // {keys:[],values:[]}
   );
   return newKeysAndValues
 }
-function commonMap2({key, oldKey, value, pV}, currFullKey, currKey){
+function commonReduceLogic({key, oldKey, value, pV}, currFullKey, currKey){
   if (typeof value === 'object') {
     resultsObject = handleNested(value, currFullKey)
     pV = ecma2018ArrMerge(resultsObject, pV)
     // pV = mergeObjects(pV, resultsObject)
   } else if(typeof value === 'undefined') { }
   else {
-      pV = updatePv2(pV, { key, value }, currKey, oldKey );
+    pV = updatePv(pV, { key, value }, currKey, oldKey );
   }
   return pV
 }
-function updatePv2(pV, {key, value}, currKey, oldKey) {
+function updatePv(pV, {key, value}, currKey, oldKey) {
   pV[`${oldKey}${currKey}`] = value
   // pV.keys.push(oldKey + keyCallback(key)) // intentionally insert '.' for string flattening keys.
   // pV.values.push(value)
   return pV // *********  Important ******
 }
 
-// spec pV =  { keys:[], values:[] }
-function updatePv(pV, {key, value}, keyCallback, oldKey) {
-  pV[`${oldKey}${keyCallback(key)}`] = value
-  // pV.keys.push(oldKey + keyCallback(key)) // intentionally insert '.' for string flattening keys.
-  // pV.values.push(value)
-  return pV // *********  Important ******
-}
 
 function ecma2018ArrMerge(resultsObject, pV){
   // for alternatives check, https://stackoverflow.com/a/171256
@@ -94,5 +85,5 @@ console.log('outputs')
 console.log('reduce obj:', reduceObject(obj, 'obj'))
 console.log('reduce obj:', handleNested(obj, 'obj'))
 console.log()
-// console.log('reduce arr:', reduceArray(arr, 'arr'))
-// console.log('reduce arr:', handleNested(arr, 'arr'))
+console.log('reduce arr:', reduceArray(arr, 'arr'))
+console.log('reduce arr:', handleNested(arr, 'arr'))
