b9295f40bec036f7ce344c32cb023cee58cdbe9a
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-09-27 16:01:33 -0400

    code cleanup, remove unused commonMap function in favor of commonMap2

--- flattenAnything.js
+++ flattenAnything.js
@@ -12,31 +12,24 @@ function handleNested(val, oldKey){
       : val
     )
 }
-// pV = discoverPv({pV, key, value}, oldKeyBuild(oldKey, key))
-// function discoverPv({pV, key, value}, resolvedKey){
-//  if (typeof value === 'object') {
-//    resultsObject = handleNested(value, resolvedKey)
-//    pV = ecma2018ArrMerge(resultsObject, pV)
-//    // pV = mergeObjects(pV, resultsObject)
-//  } else if(typeof value === 'undefined') { }
-//  else {
-//      pV = updatePv(pV, { key, value }, arrKeyCallback, oldKey );
-//  }
-//  return pV;
-// }
+
 function oldKeyBuildArray({oldKey, key}){
   return `${oldKey}[${key}]`
+}
+function arrKeyCallback(key){
+  return `[${key}]`
 }
 function oldKeyBuildObject({oldKey, key}){
   return `${oldKey}.${key}`
 }
+function objKeyCallback(key){
+  return `.${key}`
+}
 
 function reduceArray (arr, oldKey) { // previous Value, currentValue, currentIndex
-  console.log('arrrrrrrrray reached')
   let newKeysAndValues = arr.reduce( (pV, cV, cI) =>
     {
-      let key = cI
-      let value = cV
+      let key = cI; let value = cV;
       pV = commonMap2({key, oldKey, value, pV}, `${oldKey}[${key}]`, `[${key}]`)
       return pV
     },
@@ -46,27 +39,15 @@ function reduceArray (arr, oldKey) { // previous Value, currentValue, currentInd
   // oldKeysAndValue{ keys:[a, b, c], values:[7, 2, {}] }
   return newKeysAndValues
 }
-function commonMap({key, oldKey, value, pV}){
-  if (typeof value === 'object') {
-    resultsObject = handleNested(value, `${oldKey}[${key}]`)
-    pV = ecma2018ArrMerge(resultsObject, pV)
-    // pV = mergeObjects(pV, resultsObject)
-  } else if(typeof value === 'undefined') { }
-  else {
-      pV = updatePv(pV, { key, value }, arrKeyCallback, oldKey );
-  }
-  return pV
-}
 
 function reduceObject (obj, oldKey) { // previous Value, currentValue, currentIndex
   let newKeysAndValues = Object.keys(obj).reduce( (pV, cV, cI) =>
     {
-      let key = cV
-      let value = obj[cV]
+      let key = cV; let value = obj[cV]
       pV = commonMap2({key, oldKey, value, pV}, `${oldKey}.${key}`, `.${key}`)
       return pV
     },
-    { }
+    {}
   );
   return newKeysAndValues
 }
@@ -94,13 +75,6 @@ function updatePv(pV, {key, value}, keyCallback, oldKey) {
   // pV.keys.push(oldKey + keyCallback(key)) // intentionally insert '.' for string flattening keys.
   // pV.values.push(value)
   return pV // *********  Important ******
-}
-
-function objKeyCallback(key){
-  return '.' + key
-}
-function arrKeyCallback(key){
-  return `[${key}]`
 }
 
 function ecma2018ArrMerge(resultsObject, pV){
