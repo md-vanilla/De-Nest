e5ca0ab02f02275b0ec57d36391e918ac1330dc2
michael dimmitt <michaelgdimmitt@gmail.com>
2019-03-16 00:46:46 -0400

    Convert the result object to key and value arrays.

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const { describe, it } = require('mocha');
-const { handleNested,reduceArray,reduceObject, commonReduceLogic } = require('./de_nest.js')
-const { varInfo, updatePv, ecma2015ArrMerge } = require('./js_nest_helpers.js')
+const { flattenAnything, handleNested,reduceArray,reduceObject, commonReduceLogic } = require('./de_nest.js')
+const { varInfo, updatePv, ecma2015ArrMerge, convertObjectToArrays } = require('./js_nest_helpers.js')
 
 const obj = { a: 7, b: 2, c: {value:3} };
 describe('sanity', () => {
@@ -18,12 +18,23 @@ describe('de-nest is working properly for', () => {
     const arr = [7, undefined,  {value: undefined, value2: 4}, 1, 2, 3, 4];
     const obj = { a: 7, b: 2, c: {value:3} };
 
+    it('flattenAnything, evaluate an array as an array', () => {
+      assert.equal(
+        JSON.stringify(flattenAnything({arr})),
+        '{"keys":["arr[0]","arr[2].value2","arr[3]","arr[4]","arr[5]","arr[6]"],"values":[7,4,1,2,3,4]}'
+      );
+    })
+
     it('handleNested, evaluate an array as an array', () => {
-      assert.equal(JSON.stringify(handleNested({arr})),'{"arr[0]":7,"arr[2].value2":4,"arr[3]":1,"arr[4]":2,"arr[5]":3,"arr[6]":4}' );
+      assert.equal(
+        JSON.stringify(handleNested({arr})),
+        '{"arr[0]":7,"arr[2].value2":4,"arr[3]":1,"arr[4]":2,"arr[5]":3,"arr[6]":4}'
+      );
     })
 
     it('reduce array method should give array syntax top level with either as it nests deeper', () => {
-      assert.equal(JSON.stringify(reduceArray(['arr', arr])),'{"arr[0]":7,"arr[2].value2":4,"arr[3]":1,"arr[4]":2,"arr[5]":3,"arr[6]":4}' );
+      assert.equal(JSON.stringify(reduceArray(['arr', arr])),
+        '{"arr[0]":7,"arr[2].value2":4,"arr[3]":1,"arr[4]":2,"arr[5]":3,"arr[6]":4}' );
     })
 
     it('reduce a array method should not give object syntax top level', () => {
@@ -38,6 +49,10 @@ describe('de-nest is working properly for', () => {
 
     const obj = { a: 7, b: 2, c: {value:3} };
     const arr = [7, undefined,  {value: undefined, value2: 4}, 1, 2, 3, 4];
+
+    it('flattenAnything, evaluate an object as an object', () => {
+      assert.equal(JSON.stringify(flattenAnything({obj})),'{"keys":["obj.a","obj.b","obj.c.value"],"values":[7,2,3]}' );
+    })
 
     it('handleNested, evaluate an object as an object', () => {
       assert.equal(JSON.stringify(handleNested({obj})),'{"obj.a":7,"obj.b":2,"obj.c.value":3}' );
@@ -59,5 +74,10 @@ describe('de-nest is working properly for', () => {
     it('two objects together', () => {
       assert.equal(JSON.stringify(ecma2015ArrMerge(mergeObj1, mergeObj2)), '{"c":{"value":3},"d":2,"a":7,"b":2}');
     })
+  })
+
+  describe('#convert', () => {
+    const obj = {"obj.a":7,"obj.b":2,"obj.c.value":3}
+    assert.equal(JSON.stringify(convertObjectToArrays(obj)),'{"keys":["obj.a","obj.b","obj.c.value"],"values":[7,2,3]}')
   })
 })
